6845caa7296cc4f01b1cf9b7877696500af7fbcb
romainsessa <romainsessa@gmail.com>
2025-09-08 13:55:58 +0200

    init repo with existing api

---
+++ src/main/java/fr/m2i/api/HomeController.java
@@ -0,0 +1,19 @@
+package fr.m2i.api;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+	@GetMapping("/")
+	public String getHello() {
+		return "Hello";
+	}
+	
+	@GetMapping("/admin")
+	public String getHelloAdmin() {
+		return "Hello admin";
+	}
+	
+}
